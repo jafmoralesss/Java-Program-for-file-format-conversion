11da711ed47b1a7e02fe86a34a8b103c927c07f6
jafmoralesss <jafetmoralesmartinez@gmail.com>
2025-09-25 20:31:20 -0600

    OFFICIAL CONVERTER
    This is the official functional Json-to-Csv cinverter. New versions will be uploaded with the next updates

--- JSONtoCSV/src/main/java/org/example/MainConverter.java
+++ JSONtoCSV/src/main/java/org/example/MainConverter.java
@@ -4,23 +4,38 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ *  This class converts JSON files into CSV files.
+ *  This version doesn't support nested objects in other objects.
+ *  Version established as:
+ * @version 1.0.0
+ * @author Jafet Morales NAO ID: 3357
+ */
 public class MainConverter {
-    public static void main(String[] args) {
 
+    /**
+     * This method manages the user interaction by using Scanner to generate the file conversion by
+     * calling ReadJson and WriteCsv.
+     *
+     * @param args Not used in this version.
+     */
+    public static void main(String[] args) {
+        System.out.println("===WELCOME TO THE JSON-TO-CSV CONVERTER===");
         Scanner scannedFilePath = new Scanner(System.in);
-        System.out.println("Insert your JSON FilePath: ");
+        System.out.println("Insert your JSON file's FilePath: ");
         String inputFilePath = scannedFilePath.nextLine();
-        System.out.println("Insert your CSV output FilePath: ");
+        System.out.println("Insert the FilePath for your CSV file: ");
         String outputFilePath = scannedFilePath.nextLine();
 
         ReadJson startReadJson = new ReadJson();
         List<Map<String, Object>> parsedData = startReadJson.parseJsonFile(inputFilePath);
 
-        if (parsedData != null) {
+        if (parsedData!=null) {
             WriteCsv csvWriter = new WriteCsv();
             csvWriter.writeCsvFile(parsedData, outputFilePath);
+            System.out.println("SUCCESFUL CONVERSION: Check your output filepath!");
         } else {
-            System.out.println("Empty file or couldn't be processed.");
+            System.out.println("ERROR: File couldn't be processed or it's empty");
         }
     }
 }
--- JSONtoCSV/src/main/java/org/example/ReadJson.java
+++ JSONtoCSV/src/main/java/org/example/ReadJson.java
@@ -3,8 +3,10 @@ package org.example;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.LinkedHashMap;
 
 public class ReadJson {
 
@@ -16,14 +18,13 @@ public class ReadJson {
      */
     public List<Map<String, Object>> parseJsonFile(String filePath) {
         List<Map<String, Object>> data = null;
-        try{
+        try {
             ObjectMapper objectMapper = new ObjectMapper();
             data = objectMapper.readValue(new File(filePath), List.class);
         } catch (IOException e) {
             e.printStackTrace();
             System.err.println("ERROR: File not found or could not be read");
         }
-
         return data;
     }
 }
